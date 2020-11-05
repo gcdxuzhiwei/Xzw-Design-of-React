654310dfcd2c336251d5eac5c8f8db2fe8b666f5
xuzhiwei <xuzhiwei4347@qq.com>
2020-11-05 16:24:18 +0800

    demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MouseTracker from "./components/MouseTracker";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <MouseTracker />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/LikeButton.tsx
@@ -0,0 +1,29 @@
+import React, { useState,useEffect } from "react";
+
+const LikeButton: React.FC = () => {
+  const [like, setLike] = useState(0);
+  const [on, setOn] = useState(true);
+  useEffect(()=>{
+    document.title=`点击了${like}次`
+  })
+  return (
+    <>
+      <button
+        onClick={() => {
+          setLike(like + 1);
+        }}
+      >
+        {like}+
+      </button>
+      <button
+        onClick={() => {
+          setOn(!on);
+        }}
+      >
+        {on ? "on" : "off"}
+      </button>
+    </>
+  );
+};
+
+export default LikeButton;
