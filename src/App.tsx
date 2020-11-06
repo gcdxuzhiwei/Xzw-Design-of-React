dc695faa7995d58d19e857ecd7a5993310fa563c
xuzhiwei <xuzhiwei4347@qq.com>
2020-11-06 23:19:46 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import MouseTracker from "./components/MouseTracker";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <MouseTracker />
         <a
           className="App-link"
           href="https://reactjs.org"
